001247b1231482a4fc099f4a6a3961fd60ac5aea
Vincent Labatut <vince.labatut@gmail.com>
2017-01-13 18:33:11 +0100

    updated graphics

--- src/rscripts/strength-evol.R
+++ src/rscripts/strength-evol.R
@@ -22,20 +22,30 @@ library("igraph")		# to handle graphs
 
 # parameters
 ###############################################################################
-series.name <- "BB"										# TODO name of the series to process
+#series.name <- "BB"										# TODO name of the series to process
 #series.name <- "GoT"
-#series.name <- "HoC"
+series.name <- "HoC"
 if(series.name=="BB")
-{	# TODO names of the characters to plot
+{	# TODO last scene limit (or NA for all scenes), as numbered in the graph file names
+#	last.scene <- NA	# Everything
+	last.scene <- 400	# S1-S2
+	# TODO names of the characters to plot
 	char.names <- list(											
 		c("Walter White","Tuco Salamanca"),
 		c("Walter White","Jesse Pinkman"),
 		c("Walter White","Saul Goodman","Mike Ehrmantraut"),
 		c("Walter White","Skyler White","Walter White Jr.")
 	)
+	# TODO periods displayed in the character plot (possibly none)
+	char.periods <- list(
+		list(),
+		list(),
+		list(),
+		list()
+	)
 	# TODO vertical marks displayed in the character plot (possibly none)
 	char.marks <- list(
-		list("Tuco's death"=135),
+		list("Tuco's death "=172),	# verified
 		list(),
 		list(),
 		list()
@@ -44,37 +54,66 @@ if(series.name=="BB")
 	link.names <- list(
 		c("Walter White","Jesse Pinkman")
 	)
+	# TODO periods displayed in the link plot (possibly none)
+	link.periods <- list(
+		list()
+	)
 	# TODO vertical marks displayed in the link plot (possibly none)
 	link.marks <- list(
 		list()
 	)
 }else if(series.name=="GoT")
-{	# TODO names of the characters to plot
+{	# TODO last scene limit (or NA for all scenes), as numbered in the graph file names
+#	last.scene <- NA
+	last.scene <- 457	# S1-S2
+	# TODO names of the characters to plot
 	char.names <- list(
 		c("Daenerys Targaryen","Tyrion Lannister"),
 		c("Rickon Stark","Catelyn Stark","Brandon Stark","Eddard Stark","Sansa Stark","Arya Stark","Robb Stark","Jon Snow"),
 		c("Tywin Lannister","Jaime Lannister","Cersei Lannister","Tyrion Lannister")
 	)
+	# TODO periods displayed in the character plot (possibly none)
+	char.periods <- list(
+		list(),#list("Test situation"=c(100,140)),
+		list(),
+		list()
+	)
 	# TODO vertical marks displayed in the character plot (possibly none)
 	char.marks <- list(
-		list("Tyrion becomes the King's Hand"=220),
+		list("Tyrion becomes the King's Hand "=252),	# verified
 		list(),
 		list()
 	)
 	# TODO names of the relationships to plot
 	link.names <- list(
-		c("Jaime Lannister","Cersei Lannister","Jaime Lannister","Tyrion Lannister","Cersei Lannister","Tyrion Lannister")
+		c("Jaime Lannister","Cersei Lannister","Jaime Lannister","Tyrion Lannister","Cersei Lannister","Tyrion Lannister"),
+		c("Jon Snow","Ygritte")
+	)
+	# TODO periods displayed in the link plot (possibly none)
+	link.periods <- list(
+		list(),
+		list("Romantic relationship"=c(100,400))
 	)
 	# TODO vertical marks displayed in the link plot (possibly none)
 	link.marks <- list(
-		list("Tyrion becomes the King's Hand"=220)
+		list("Tyrion becomes the King's Hand "=252),	# verified
+		list()
 	)
 }else if(series.name=="HoC")
-{	# TODO names of the characters to plot
+{	# TODO last scene limit (or NA for all scenes), as numbered in the graph file names
+#	last.scene <- NA
+	last.scene <- 879	# S1-S2 (not all S2, just the scenes present in all 3 types of graphs)
+	# TODO names of the characters to plot
 	char.names <- list(
 		c("Francis Underwood","Claire Underwood"),
 		c("Francis Underwood","Garrett Walker"),
 		c("Zoe Barnes","Lucas Goodwin")
+	)
+	# TODO periods displayed in the character plot (possibly none)
+	char.periods <- list(
+		list(),
+		list(),
+		list()
 	)
 	# TODO vertical marks displayed in the character plot (possibly none)
 	char.marks <- list(
@@ -86,9 +125,13 @@ if(series.name=="BB")
 	link.names <- list(
 		c("Francis Underwood","Claire Underwood","Francis Underwood","Martin Spinella","Lucas Goodwin","Gavin Orsay")
 	)
+	# TODO periods displayed in the link plot (possibly none)
+	link.periods <- list(
+		list()
+	)
 	# TODO vertical marks displayed in the link plot (possibly none)
 	link.marks <- list(
-		list("F.Underwood/M.Spinella fight"=125)
+		list("F.Underwood/M.Spinella fight "=129)	# verified
 	)
 }
 
@@ -123,6 +166,7 @@ palette <- c(
 # get the character strengths
 ###############################################################################
 # process each type of network
+#for(type in c("ns"))
 for(type in c("ns","ts10","ts40"))
 {	# setup the folders
 	input.folder <- file.path(data.base,paste0(series.name,"_dyn_",type))
@@ -133,6 +177,11 @@ for(type in c("ns","ts10","ts40"))
 	scenes <- sapply(strsplit(graph.files,"[_.]",fixed=FALSE),function(s) as.integer(s[3]))
 	graph.files <- c(sort(graph.files[scenes<1000]),sort(graph.files[scenes>=1000]))
 	scenes <- sapply(strsplit(graph.files,"[_.]",fixed=FALSE),function(s) as.integer(s[3]))
+	if(!is.na(last.scene))
+	{	idx <- which(scenes==last.scene)	
+		scenes <- scenes[1:idx]
+		graph.files <- graph.files[1:idx]
+	}
 	
 	# load the graphs, including the link weights
 	###############################################################################
@@ -178,26 +227,47 @@ for(type in c("ns","ts10","ts40"))
 	###############################################################################
 	for(grp in 1:length(char.names))
 	{	pdf.file <- file.path(res.folder,paste0("strengh_grp",grp,"_",type,".pdf"))
-		pdf(file=pdf.file)	
+		pdf(file=pdf.file)
+			# create the empty plot 
 #			plot(x=1:length(graph.files),y=rep(0,length(graph.files)),
 			plot(x=scenes,y=rep(0,length(graph.files)),
 					ylim=c(min(char.str[[grp]]),max(char.str[[grp]])),
 					type="n",
 					xlab="Scene rank",
+#					xaxt="n",
 					ylab="Speaker strength")
-			if(length(char.marks[[grp]]>0))
+			# add period parks
+			if(length(char.periods[[grp]])>0)
+			{	for(i in 1:length(char.periods[[grp]]))
+#				{	start.scene <- char.periods[[grp]][[i]][1]
+#					end.scene <- char.periods[[grp]][[i]][2]
+				{	start.scene <- which(scenes==char.periods[[grp]][[i]][1])
+					end.scene <- which(scenes==char.periods[[grp]][[i]][2])
+					rect(xleft=start.scene, xright=end.scene, 
+							ybottom=grconvertY(0,from='npc'), ytop=grconvertY(1,from='npc'),
+							col="GREY80",border=NA)
+					text(x=(start.scene+end.scene)/2, y=max(char.str[[grp]]),
+							labels=names(char.periods[[grp]])[i],
+							#adj=NULL, pos=NULL, offset=0.5,
+							pos=3, #offset=0.25,
+							cex=0.5, col="GREY30")
+				}
+			}
+			# add punctual marks
+			if(length(char.marks[[grp]])>0)
 			{	for(i in 1:length(char.marks[[grp]]))
 #				{	scene <- char.marks[[grp]][i]
-				{	scene <- which(scenes==char.marks[[grp]][i])
+				{	scene <- which(scenes==char.marks[[grp]][[i]])
 					abline(v=scene,
 							col="GREY30", lwd=2) #, lty=3
-					text(x=scene, y=max(char.str[[grp]]), 
+					text(x=scene, y=grconvertY(1,from='npc'), 
 							labels=names(char.marks[[grp]])[i],
 							#adj=NULL, pos=NULL, offset=0.5,
 							pos=2, offset=0.25,
 							srt=90, cex=0.5, col="GREY30")
 				}
 			}
+			# add the data series
 			for(i in 1:nrow(char.str[[grp]]))
 #			{	lines(x=1:length(graph.files),y=char.str[[grp]][i,],
 			{	lines(x=scenes,y=char.str[[grp]][i,],
@@ -206,35 +276,61 @@ for(type in c("ns","ts10","ts40"))
 					col=palette[i]
 				)
 			}
+			# add the legend
 			legend(x="topright",
 					legend=char.names[[grp]],
 					fill=palette[1:nrow(char.str[[grp]])]
-				)
+			)
+			
+			# re-draw the bounding box border (on top of the possibly interfering graphics)
+			rect(xleft=grconvertX(0,from='npc'), xright=grconvertX(1,from='npc'), 
+					ybottom=grconvertY(0,from='npc'), ytop=grconvertY(1,from='npc'),
+					col=NA,border="BLACK",xpd=TRUE)
 		dev.off()
 	}
 
 	for(grp in 1:length(link.names))
 	{	pdf.file <- file.path(res.folder,paste0("weight_grp",grp,"_",type,".pdf"))
 		pdf(file=pdf.file)	
+			# create the empty plot 
 #			plot(x=1:length(graph.files),y=rep(0,length(graph.files)),
 			plot(x=scenes,y=rep(0,length(graph.files)),
 					ylim=c(min(link.str[[grp]]),max(link.str[[grp]])),
 					type="n",
 					xlab="Scene rank",
 					ylab="Relation weight")
-			if(length(link.marks[[grp]]>0))
+			# add period parks
+			if(length(link.periods[[grp]])>0)
+			{	for(i in 1:length(link.periods[[grp]]))
+#				{	start.scene <- link.periods[[grp]][[i]][1]
+#					end.scene <- link.periods[[grp]][[i]][2]
+				{	start.scene <- which(scenes==link.periods[[grp]][[i]][1])
+					end.scene <- which(scenes==link.periods[[grp]][[i]][2])
+					rect(xleft=start.scene, xright=end.scene, 
+							ybottom=grconvertY(0,from='npc'), ytop=grconvertY(1,from='npc'),
+							col="GREY80",border=NA)
+					text(x=(start.scene+end.scene)/2, y=max(link.str[[grp]]),
+							labels=names(link.periods[[grp]])[i],
+							#adj=NULL, pos=NULL, offset=0.5,
+							pos=3, #offset=0.25,
+							cex=0.5, col="GREY30")
+				}
+			}
+			# add punctual marks
+			if(length(link.marks[[grp]])>0)
 			{	for(i in 1:length(link.marks[[grp]]))
-#				{	scene <- link.marks[[grp]][i]
-				{	scene <- which(scenes==link.marks[[grp]][i])
+#				{	scene <- link.marks[[grp]][[i]]
+				{	scene <- which(scenes==link.marks[[grp]][[i]])
 					abline(v=scene,
 							col="GREY30", lwd=2) #, lty=3
-					text(x=scene, y=max(link.str[[grp]]), 
+					text(x=scene, y=grconvertY(1,from='npc'), 
 							labels=names(link.marks[[grp]])[i],
 							#adj=NULL, pos=NULL, offset=0.5,
 							pos=2, offset=0.25,
 							srt=90, cex=0.5, col="GREY30")
 				}
 			}
+			# add the data series
 			for(i in 1:nrow(link.str[[grp]]))
 #			{	lines(x=1:length(graph.files),y=link.str[[grp]][i,],
 			{	lines(x=scenes,y=link.str[[grp]][i,],
@@ -243,12 +339,19 @@ for(type in c("ns","ts10","ts40"))
 						col=palette[i]
 				)
 			}
+			# add the legend
 			legend(x="topright",
 					legend=rownames(link.str[[grp]]),
 					fill=palette[1:nrow(link.str[[grp]])]
 			)
+			
+			# re-draw the bounding box border (on top of the possibly interfering graphics)
+			rect(xleft=grconvertX(0,from='npc'), xright=grconvertX(1,from='npc'), 
+					ybottom=grconvertY(0,from='npc'), ytop=grconvertY(1,from='npc'),
+					col=NA,border="BLACK",xpd=TRUE)
 		dev.off()
 	}
 }
 
-#TODO check jon vs. ygritte
+#TODO afficher saisons & épisodes en x ?
+#TODO afficher période au lieu de points d'intérêt ?
